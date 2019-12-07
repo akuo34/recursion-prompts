d020da1c6f85854ac0d49dd59f9705e9a0f2dce1
akuo34 <akuo34@gmail.com>
2019-12-07 02:12:46 -0800

    Edit recursion.js complete 1 thru 5 tests

--- src/recursion.js
+++ src/recursion.js
@@ -7,26 +7,67 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+    if (n < 0) {
+        return null;
+    }
+    if (n === 0) {
+        return 1;
+    }
+    return n*(factorial(n-1));
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    var arraycopy = array.slice();
+    if (arraycopy.length === 0) {
+        return 0;
+    }
+
+    return arraycopy.pop() + sum(arraycopy);
+      
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      sum += arraySum(array[i]);
+    } else {
+      sum += array[i];
+    }
+  }
+  return sum;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  var absnum = Math.abs(n);
+  if (absnum >= 2) {
+      absnum -= 2;
+      return isEven(absnum);
+  } else if (absnum === 1) {
+      return false;
+  } else {
+      return true;
+  }
+
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n === 0) {
+    return 0;
+  }
+  if (n > 0) {
+    return (n - 1) + sumBelow(n - 1);
+  } else {
+    return (n + 1) + sumBelow(n + 1);
+  }
 };
 
 // 6. Get the integers within a range (x, y).
